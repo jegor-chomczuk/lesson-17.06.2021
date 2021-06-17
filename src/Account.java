3a2c5907c78f7f9857ce8bc567ae05eec2fd46ee
Jegor <jegor.chomczuk@gmail.com>
2021-06-17 21:39:50 +0200

    First commit

---
+++ src/Account.java
@@ -0,0 +1,15 @@
+public class Account {
+    private String name;
+    private String address;
+    private String accountNumber;
+    private double balance;
+
+    public Account(String name, String address, String accountNumber, double balance) {
+        this.name = name;
+        this.address = address;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+
+}
